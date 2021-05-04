7c1c55b26bc77ecc2e813ed9c6f3155c751f6299
Oreshkov Vladimir <voresh@inbox.ru>
2021-05-04 18:29:40 +0300

    waves 2

--- Assets/Scripts/BotSpawner.cs
+++ Assets/Scripts/BotSpawner.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class BotSpawner : JamBase<BotSpawner> {
     public Bot BotPrefab;
     public float SpawnRate = 20f;
-    public Collider TriggerCollider;
+    public List<Collider> SpawnPoints;
     public int CurrentWave = -1;
+    public int BotsCount = 3; //todo: moar complicated logic
+    
     public float NextWaveTime => SpawnRate - (Time.time - _LastWaveTime);
-        
     public event Action<int> onWaveSpawnStarted = _ => { };
     
     private float _LastWaveTime;
@@ -20,11 +23,14 @@ public class BotSpawner : JamBase<BotSpawner> {
         if (Time.time - _LastWaveTime < SpawnRate)
             return;
         CurrentWave++;
-        var bounds = TriggerCollider.bounds;
-        var botInstance = Instantiate(BotPrefab, new Vector3(UnityEngine.Random.Range(bounds.min.x, bounds.max.x), UnityEngine.Random.Range(bounds.min.y, bounds.max.y), UnityEngine.Random.Range(bounds.min.z, bounds.max.z)), Quaternion.identity);
-        var botHealth = botInstance.GetComponent<Health>();
-        botHealth.HealthMax = 10;
-        botHealth.HealthCurrent = 10;
+        var bounds = SpawnPoints[UnityEngine.Random.Range(0, SpawnPoints.Count)].bounds;
+        for (var i = 0; i < BotsCount; i++) {
+            var position = new Vector3(UnityEngine.Random.Range(bounds.min.x, bounds.max.x), UnityEngine.Random.Range(bounds.min.y, bounds.max.y), UnityEngine.Random.Range(bounds.min.z, bounds.max.z));
+            var botInstance = Instantiate(BotPrefab, position, Quaternion.identity);
+            var botHealth = botInstance.GetComponent<Health>();
+            botHealth.HealthMax = 10;
+            botHealth.HealthCurrent = 10;
+        }
         _LastWaveTime = Time.time;
         onWaveSpawnStarted.Invoke(CurrentWave);
     }
