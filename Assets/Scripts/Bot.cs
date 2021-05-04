a003a65b95b57c0440ae2bf342eaa8f175befe06
MoldyDanya <Danchikpunk@gmail.com>
2021-05-04 16:57:15 +0300

    Первая итерация следования

--- Assets/Scripts/Bot.cs
+++ Assets/Scripts/Bot.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 public class Bot : MonoBehaviour {
     public BulletSpawner BulletSpawner;
     public Animator Animator;
+    public float Speed = 2.5f;
     public float DetectionRadius = 7f;
+    public float AttackRadius = 4f;
+    private static readonly int _Speed = Animator.StringToHash("Speed");
     public GameObject Target;
     public Health Health;
 
@@ -28,7 +31,15 @@ public class Bot : MonoBehaviour {
         if (Health.HealthCurrent <= 0)
             return;
         var direction = Target.transform.position - transform.position;
-        if (Target.GetComponent<Health>().HealthCurrent > 0 && direction.magnitude < DetectionRadius) {
+        if (Target.GetComponent<Health>().HealthCurrent > 0 && direction.magnitude < DetectionRadius && direction.magnitude > AttackRadius) {
+            transform.position += direction.normalized * Speed * Time.deltaTime;
+            transform.forward = direction;
+            Animator.SetFloat(_Speed, Speed);
+        }
+        else {
+            Animator.SetFloat(_Speed, 0f);
+        }
+        if (Target.GetComponent<Health>().HealthCurrent > 0 && direction.magnitude < AttackRadius) {
             BulletSpawner.enabled = true;
             transform.forward = direction;
         }
