9bdaac05f8eafb4ab765f51d9bc7baa38757b498
Oreshkov Vladimir <voresh@inbox.ru>
2021-05-04 19:06:18 +0300

    announcements wip

---
+++ Assets/Scripts/Widgets/AnnouncementController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnnouncementController : JamBase<AnnouncementController> {
+    public Text Text;
+    public CanvasGroup Group;
+    public float AnnouncementDuration = 2f;
+    public AnimationCurve AlphaCurve;
+    
+    private Queue<string> _Scheduled = new Queue<string>();
+    private bool _InProgress;
+    private string _CurrentText;
+    private float _LastAnnouncementTime;
+
+    private void Start() {
+        Text.text = string.Empty;
+    }
+
+    public void Schedule(string text) {
+        _Scheduled.Enqueue(text);
+    }
+
+    private void Update() {
+        var timePast = Time.time - _LastAnnouncementTime;
+        if (timePast < AnnouncementDuration) {
+            if (_InProgress) {
+                Text.text = _CurrentText;
+                Group.alpha = AlphaCurve.Evaluate(timePast / AnnouncementDuration);
+            }
+        }
+        else {
+            if (_Scheduled.Count > 0) {
+                _CurrentText = _Scheduled.Dequeue();
+                _InProgress = true;
+            }
+            else {
+                _InProgress = false;
+            }
+        }
+    }
+}
