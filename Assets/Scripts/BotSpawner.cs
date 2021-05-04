049e61b81af4123a8080e47bfa5c3898bcd9e3b8
Oreshkov Vladimir <voresh@inbox.ru>
2021-05-04 18:17:49 +0300

    waves

--- Assets/Scripts/BotSpawner.cs
+++ Assets/Scripts/BotSpawner.cs
@@ -3,19 +3,29 @@ using UnityEngine;
 
 public class BotSpawner : JamBase<BotSpawner> {
     public Bot BotPrefab;
-    public float SpawnRate = 2f;
+    public float SpawnRate = 20f;
     public Collider TriggerCollider;
-
-    private float _LastSpawnTime;
+    public int CurrentWave = -1;
+    public float NextWaveTime => SpawnRate - (Time.time - _LastWaveTime);
+        
+    public event Action<int> onWaveSpawnStarted = _ => { };
     
+    private float _LastWaveTime;
+
+    private void Start() {
+        _LastWaveTime = Time.time;
+    }
+
     private void Update() {
-        if (Time.time - _LastSpawnTime < SpawnRate)
+        if (Time.time - _LastWaveTime < SpawnRate)
             return;
+        CurrentWave++;
         var bounds = TriggerCollider.bounds;
         var botInstance = Instantiate(BotPrefab, new Vector3(UnityEngine.Random.Range(bounds.min.x, bounds.max.x), UnityEngine.Random.Range(bounds.min.y, bounds.max.y), UnityEngine.Random.Range(bounds.min.z, bounds.max.z)), Quaternion.identity);
         var botHealth = botInstance.GetComponent<Health>();
         botHealth.HealthMax = 10;
         botHealth.HealthCurrent = 10;
-        _LastSpawnTime = Time.time;
+        _LastWaveTime = Time.time;
+        onWaveSpawnStarted.Invoke(CurrentWave);
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,12 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class GameController : JamBase<GameController> {
     public void Start() {
         PlayerController.Instance.onDied += OnPlayerDied;
+        BotSpawner.Instance.onWaveSpawnStarted += OnWaveSpawnStarted;
     }
 
     private void OnPlayerDied() {
         Debug.Log("end game");
         BotSpawner.Instance.enabled = false;
+    }
+
+    private void OnWaveSpawnStarted(int wave) {
+        Debug.Log($"next wave {wave + 1}");
+        //todo: notification here
+    }
+
+    private void Update() {
+        //todo: update wave time here
     }
 }
