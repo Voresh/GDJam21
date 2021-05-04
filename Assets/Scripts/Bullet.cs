5839e568bfc25ca8547facf064f14c28d2eca510
Oreshkov Vladimir <voresh@inbox.ru>
2021-05-04 15:25:00 +0300

    shooting

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    public float Speed = 10f;
+    public float Lifetime = 10f;
+    public GameObject Owner;
+    private float _SpawnTime;
+
+    private void Start() {
+        _SpawnTime = Time.time;
+    }
+
+    private void Update() {
+        transform.position += transform.forward * Speed * Time.deltaTime;
+        if (Time.time - _SpawnTime > Lifetime)
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (Owner == other.gameObject)
+            return;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour {
+    public float ShootRate = 0.5f;
+    public Bullet Bullet;
+    public Transform ShootPosition;
+    public Transform Shooter;
+
+    private float _LastShotTime;
+    
+    private void Update() {
+        if (Time.time - _LastShotTime < ShootRate)
+            return;
+        var bulletInstance = Instantiate(Bullet);
+        bulletInstance.transform.position = ShootPosition.position;
+        bulletInstance.transform.forward = Shooter.forward;
+        bulletInstance.Owner = Shooter.gameObject;
+        _LastShotTime = Time.time;
+    }
+}
