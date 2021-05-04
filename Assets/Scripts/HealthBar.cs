a1e792d4eb4b38fe22f982975f179bbc507a1b26
Oreshkov Vladimir <voresh@inbox.ru>
2021-05-04 17:07:14 +0300

    health rotation, camera setup

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,10 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour {
     public Image FillImage;
 
+    public Transform RotationTarget;
+    
     public float Fill {
         set => FillImage.fillAmount = value;
+    }
+
+    private void Start() {
+        RotationTarget = CameraController.Instance.transform;
+    }
+
+    private void Update() {
+        if (RotationTarget != null) {
+            transform.forward = RotationTarget.transform.position - transform.position;
+            var eulerRotation = transform.rotation.eulerAngles;
+            transform.rotation = Quaternion.Euler(-90f, 180f, eulerRotation.z);
+        }
     }
 }
