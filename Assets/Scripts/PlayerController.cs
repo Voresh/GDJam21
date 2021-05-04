55e0c32c96656788332bae691fa084a95aef7d18
Oreshkov Vladimir <voresh@inbox.ru>
2021-05-04 14:24:08 +0300

    map and player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlayerController : JamBase<PlayerController> {
+    private Transform _Transform;
+    public Vector3 Position => _Transform.position;
+
+    protected override void Awake() {
+        base.Awake();
+        _Transform = transform;
+    }
+}
