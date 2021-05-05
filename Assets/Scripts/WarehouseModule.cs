d1166c5dda2cb3dfeb159418dcc1a99b8b4ef9db
MoldyDanya <Danchikpunk@gmail.com>
2021-05-05 13:21:11 +0300

    Ближе к истине

---
+++ Assets/Scripts/WarehouseModule.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+public class WarehouseModule : Module {
+    public GameObject DropPoint;
+    public GameObject[] DropItems;
+
+    public void Start() {
+        BotSpawner.Instance.onWaveStatusUpdated += OnWaveStatusUpdated;
+    }
+
+    private void OnWaveStatusUpdated(bool active)
+    {
+        if (!active)
+            Instantiate(DropItems[Random.Range(0, DropItems.Length - 1);
+            AnnouncementController.Instance.Schedule($"Wave complete");
+    }
+
+    //protected override void UpdateEffects(bool active)
+    //{
+    //    if (active)
+    //    {
+
+    //    }
+    //    else
+    //    {
+    //        return;
+    //    }
+    //}
+}
+
