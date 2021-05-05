b7d161f8f40f5c18e8957b7b870ec86a5ff843ae
Oreshkov Vladimir <voresh@inbox.ru>
2021-05-05 13:16:05 +0300

    security module

---
+++ Assets/Scripts/SecurityBot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SecurityBot : MonoBehaviour {
+    public List<Transform> Route { get; set; }
+
+    public float StoppingDistance;
+    public int TargetPointIndex;
+    public NavMeshAgent NavMeshAgent;
+    public Animator Animator;
+
+    private void Start() {
+        NavMeshAgent = GetComponent<NavMeshAgent>();
+        Animator = GetComponent<Animator>();
+        NavMeshAgent.autoBraking = false;
+        NavMeshAgent.SetDestination(Route[TargetPointIndex].position);
+        Animator.SetFloat("Speed", 1f); //todo: kek
+        GetComponent<BulletSpawner>().enabled = false;
+    }
+
+    private void Update() {
+        if ((Route[TargetPointIndex].position - transform.position).sqrMagnitude > StoppingDistance * StoppingDistance)
+            return;
+        TargetPointIndex++;
+        if (TargetPointIndex == Route.Count)
+            TargetPointIndex = 0;
+        NavMeshAgent.SetDestination(Route[TargetPointIndex].position);
+        Animator.SetFloat("Speed", 1f); //todo: kek
+    }
+}
---
+++ Assets/Scripts/SecurityModule.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityModule : Module {
+    private Coroutine _SpawnCoroutine;
+    public SecurityBot SecurityBotPrefab;
+    public List<Transform> Route;
+    public Transform SpawnPoint;
+    public float SpawnRate = 10f;
+
+    protected override void UpdateEffects(bool active) {
+        if (active) {
+            _SpawnCoroutine = StartCoroutine(SpawnSecurityBotCoroutine());
+        }
+        else {
+            if (_SpawnCoroutine != null)
+                StopCoroutine(_SpawnCoroutine);
+        }
+    }
+
+    private IEnumerator SpawnSecurityBotCoroutine() {
+        while (true) {
+            yield return new WaitForSecondsRealtime(SpawnRate);
+            var securityBot = Instantiate(SecurityBotPrefab, SpawnPoint.position, Quaternion.identity);
+            securityBot.Route = Route;
+        }
+    }
+}
