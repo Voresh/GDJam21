ce96e41f050b66ab39f9ad599f70f97d5686bf26
Oreshkov Vladimir <voresh@inbox.ru>
2021-05-05 18:01:08 +0300

    lab widgets wip

---
+++ Assets/Scripts/Widgets/Laboratory/LaboratoryButtonWidget.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LaboratoryButtonWidget : MonoBehaviour {
+    public Button Button;
+    public LaboratoryWidget LaboratoryWidget;
+
+    private void Start() {
+        Button.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick() {
+        LaboratoryWidget.gameObject.SetActive(true);
+    }
+}
