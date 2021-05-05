459cbe3946c1e6885af29827fd407bf1c0bd9bf2
MoldyDanya <Danchikpunk@gmail.com>
2021-05-05 13:45:16 +0300

    Спаун аптечек со склада готова (только спаунит не в ту точку ХД)

--- Assets/Scripts/WarehouseModule.cs
+++ Assets/Scripts/WarehouseModule.cs
@@ -4,28 +4,22 @@ using UnityEngine;
 public class WarehouseModule : Module {
     public GameObject DropPoint;
     public GameObject[] DropItems;
+    public bool Work;
 
     public void Start() {
+        Work = this.RepairedAtStart;
         BotSpawner.Instance.onWaveStatusUpdated += OnWaveStatusUpdated;
     }
 
     private void OnWaveStatusUpdated(bool active)
     {
-        if (!active)
-            Instantiate(DropItems[Random.Range(0, DropItems.Length - 1)]);
-            AnnouncementController.Instance.Schedule($"Wave complete");
+        if (!active && Work)
+            Instantiate(DropItems[Random.Range(0, DropItems.Length - 1)], DropPoint.transform);
     }
 
-    //protected override void UpdateEffects(bool active)
-    //{
-    //    if (active)
-    //    {
-
-    //    }
-    //    else
-    //    {
-    //        return;
-    //    }
-    //}
+    protected override void UpdateEffects(bool active)
+    {
+        Work = active;
+    }
 }
 
