1ea1d2783b10825529fa6d739894afeaafbcfa7c
MoldyDanya <Danchikpunk@gmail.com>
2021-05-05 17:00:57 +0300

    Залил инвалида

--- Assets/Scripts/Bot.cs
+++ Assets/Scripts/Bot.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Bot : MonoBehaviour {
     public BulletSpawner BulletSpawner;
     public Animator Animator;
+    public LayerMask EnemyLayer;
     public NavMeshAgent NavMeshAgent;
     public float Speed = 2.5f;
     public float DetectionRadius = 7f;
     public float AttackRadius = 4f;
     private static readonly int _Speed = Animator.StringToHash("Speed");
-    public GameObject Target;
+    public GameObject TargetPlayer;
+    public List<Module> TargetModules;
     public Health Health;
 
     private void Start() {
-        Target = PlayerController.Instance.gameObject;
+        TargetPlayer = PlayerController.Instance.gameObject;
+        TargetModules = ModulesController.Instance._Modules.ToList();
         NavMeshAgent = GetComponent<NavMeshAgent>();
         Health.onDeadStatusUpdated += OnDeadStatusUpdated;
         Health.RestoreHealth();
@@ -24,7 +29,6 @@ public class Bot : MonoBehaviour {
         if (dead) {
             Animator.SetBool("Died", true);
             GetComponent<Collider>().enabled = false;
-            NavMeshAgent.enabled = false;
         }
     }
 
@@ -33,8 +37,32 @@ public class Bot : MonoBehaviour {
             BulletSpawner.enabled = false;
             return;
         }
-        var direction = Target.transform.position - transform.position;
-        if (Target.GetComponent<Health>().HealthCurrent > 0 && direction.magnitude < DetectionRadius && direction.magnitude > AttackRadius) {
+
+        var targetColliders = Physics.OverlapSphere(transform.position, AttackRadius, EnemyLayer);
+        var target = targetColliders
+            .Where(_ => _.gameObject != gameObject)
+            .OrderBy(_ => Vector3.Distance(_.transform.position, transform.position))
+            .FirstOrDefault();
+        if (target != null) {
+            transform.LookAt(target.transform);
+            BulletSpawner.enabled = true;
+            NavMeshAgent.ResetPath();
+            Animator.SetFloat("Speed", 0f);
+            return;
+        }
+
+        Debug.LogError(TargetModules.ToList());
+        var RepairedModules = TargetModules
+            .Where(_ => _.GetComponent<Module>().Repaired)
+            .ToList();
+        Debug.LogError(RepairedModules);
+        var NearbyTargetModules = TargetModules
+            .OrderBy(_ => Vector3.Distance(_.transform.position, transform.position))
+            .FirstOrDefault();
+
+
+        var direction = TargetPlayer.transform.position - transform.position;
+        if (TargetPlayer.GetComponent<Health>().HealthCurrent > 0 && direction.magnitude < DetectionRadius && direction.magnitude > AttackRadius) {
             NavMeshAgent.speed = Speed;
             NavMeshAgent.Move(direction.normalized * Speed * Time.deltaTime);
             //transform.position += ;
@@ -44,7 +72,7 @@ public class Bot : MonoBehaviour {
         else {
             Animator.SetFloat(_Speed, 0f);
         }
-        if (Target.GetComponent<Health>().HealthCurrent > 0 && direction.magnitude < AttackRadius) {
+        if (TargetPlayer.GetComponent<Health>().HealthCurrent > 0 && direction.magnitude < AttackRadius) {
             BulletSpawner.enabled = true;
             transform.forward = direction;
         }
