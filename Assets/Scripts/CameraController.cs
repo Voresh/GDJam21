85ebb934d64ee11f92091598ddc9a2c542f24fd8
Oreshkov Vladimir <voresh@inbox.ru>
2021-05-06 17:27:09 +0300

    cam damp

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,29 +5,34 @@ using UnityEngine;
 public class CameraController : JamBase<CameraController> {
     public float CameraHeight;
     public float MaxCameraHeight;
-    public float CameraRange;
+    //public float CameraRange;
     public float CameraRotation;
-    public float Damping = 7f;
-    private Vector3 velocity = Vector3.zero;
+    public float Damping = 5f;
+    public float SoftDamping = 3f;
+    public float DampingDamping = 3f;
+    private float _CurrentDamping;
+    //private Vector3 velocity = Vector3.zero;
 
-    void Start()
-    {
+    void Start() {
         Quaternion.Euler(CameraRotation, 0, 0);
+        _CurrentDamping = Damping;
     }
-    
-    void Update()
-    {
-        var Target = PlayerController.Instance.NearbyTarget;
-        var PlayerPosition = PlayerController.Instance.Position;
-        var CameraPosition = new Vector3(PlayerPosition.x, PlayerPosition.y + CameraHeight, PlayerPosition.z + CameraRange);
-        var FightPosition = new Vector3(PlayerPosition.x, PlayerPosition.y + MaxCameraHeight, PlayerPosition.z + CameraRange);
-        var smoothTime = Damping * (Vector3.Distance(transform.position, CameraPosition) * Time.deltaTime);
-        if (Target != null) {
-            smoothTime = Damping * (Vector3.Distance(transform.position, FightPosition) * Time.deltaTime);
-            CameraPosition = new Vector3(PlayerPosition.x, PlayerPosition.y + MaxCameraHeight, PlayerPosition.z + CameraRange);
-        }
 
-        transform.position = Vector3.SmoothDamp(transform.position, CameraPosition, ref velocity, smoothTime);
-
+    void Update() {
+        _CurrentDamping = Mathf.Lerp(_CurrentDamping, PlayerController.Instance.NearbyTarget != null ? SoftDamping : Damping, DampingDamping * Time.deltaTime);
+        var targetPosition = PlayerController.Instance.Position
+            + Vector3.up * (PlayerController.Instance.NearbyTarget != null ? MaxCameraHeight : CameraHeight);
+        transform.position = Vector3.Slerp(transform.position, targetPosition, Time.deltaTime * _CurrentDamping);
+        //var Target = PlayerController.Instance.NearbyTarget;
+        //var PlayerPosition = PlayerController.Instance.Position;
+        //var CameraPosition = new Vector3(PlayerPosition.x, PlayerPosition.y + CameraHeight, PlayerPosition.z + CameraRange);
+        //var FightPosition = new Vector3(PlayerPosition.x, PlayerPosition.y + MaxCameraHeight, PlayerPosition.z + CameraRange);
+        //var smoothTime = Damping * (Vector3.Distance(transform.position, CameraPosition) * Time.deltaTime);
+        //if (Target != null) {
+        //    smoothTime = Damping * (Vector3.Distance(transform.position, FightPosition) * Time.deltaTime);
+        //    CameraPosition = new Vector3(PlayerPosition.x, PlayerPosition.y + MaxCameraHeight, PlayerPosition.z + CameraRange);
+        //}
+        //
+        //transform.position = Vector3.SmoothDamp(transform.position, CameraPosition, ref velocity, smoothTime);
     }
 }
