e2b86310041fbbc962c6dac016b01a491f9548f8
Oreshkov Vladimir <voresh@inbox.ru>
2021-05-06 11:04:15 +0300

    arrows

--- Assets/Scripts/Widgets/WaveArrowWidget.cs
+++ Assets/Scripts/Widgets/WaveArrowWidget.cs
@@ -1,10 +1,11 @@
 ﻿using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Widgets {
     public class WaveArrowWidget : MonoBehaviour {
         public Transform Arrow;
-        
+
         private void Update() {
             if (!BotSpawner.Instance.FirstWaveSpawned 
                 || BotSpawner.Instance.CurrentWaveBots.Count == 0 
@@ -13,10 +14,22 @@ namespace Widgets {
                 return;
             }
             Arrow.gameObject.SetActive(true);
-            var worldDirection = BotSpawner.Instance.CurrentWaveBots.First(_ => !_.Health.Dead).transform.position - PlayerController.Instance.Position;
+            var targetPosition = BotSpawner.Instance.CurrentWaveBots.First(_ => !_.Health.Dead).transform.position;
+            var targetRotation = GetRouteDirection(targetPosition);
+            Arrow.rotation = Quaternion.Slerp(Arrow.rotation, targetRotation, 7f * Time.deltaTime);
+        }
+
+        public static Quaternion GetRouteDirection(Vector3 targetPosition) {
+            var path = new NavMeshPath();
+            var pathSuccess = NavMesh.CalculatePath(PlayerController.Instance.Position, targetPosition, NavMesh.AllAreas, path) && path.corners.Length > 1;
+            var worldDirection = pathSuccess
+                ? path.corners[1] - PlayerController.Instance.Position
+                : targetPosition - PlayerController.Instance.Position;
+            //Debug.DrawRay(path.corners[1], Vector3.up * 10, Color.green, 2f);
+            //Debug.DrawRay(transform.position, worldDirection, Color.green, 2f);
+            //Debug.LogError($"{pathSuccess} {path.corners.Length}");
             var rotation = Quaternion.LookRotation(worldDirection, Vector3.up);
-            var y = rotation.eulerAngles.y;
-            Arrow.rotation = Quaternion.Slerp(Arrow.rotation, Quaternion.Euler(0f, 0f, -y), 7f * Time.deltaTime);
+            return Quaternion.Euler(0f, 0f, -rotation.eulerAngles.y);
         }
     }
 }
