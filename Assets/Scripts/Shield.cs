8bf1dcaa05c2f5531a0ac8057ad774a5e14216f6
Oreshkov Vladimir <voresh@inbox.ru>
2021-05-06 19:40:01 +0300

    shield

---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class Shield : MonoBehaviour {
+    public Health Health;
+    public GameObject Attachement;
+    
+    private void Start() {
+        Health = GetComponent<Health>();
+        Health.RestoreHealth();
+        Health.onDeadStatusUpdated += dead => {
+            if (dead)
+                Destroy(gameObject);
+        };
+    }
+
+    private void Update() {
+        if (Attachement != null) {
+            transform.SetParent(Attachement.transform);
+            transform.localPosition = Vector3.zero;
+            transform.rotation = Quaternion.identity;
+        }
+    }
+
+    private void LateUpdate() {
+        transform.rotation = Quaternion.identity;
+    }
+
+    private void FixedUpdate() {
+        transform.rotation = Quaternion.identity;
+    }
+}
---
+++ Assets/Scripts/ShieldPickup.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ShieldPickup : MonoBehaviour {
+    public Shield ShieldPrefab;
+    
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject != PlayerController.Instance.gameObject)
+            return;
+        var shield = Instantiate(ShieldPrefab);
+        shield.Attachement = PlayerController.Instance.gameObject;
+        shield.transform.localPosition = Vector3.zero;
+        Destroy(gameObject);
+    }
+}
