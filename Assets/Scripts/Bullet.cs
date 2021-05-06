0966093dbed106ba4d3c0c7619eb145e8a13a531
Oreshkov Vladimir <voresh@inbox.ru>
2021-05-06 19:45:45 +0300

    crit bullet bug fix

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -16,25 +16,32 @@ public class Bullet : MonoBehaviour {
     }
 
     private void Update() {
+        if (Time.time - _SpawnTime > Lifetime)
+            Destroy(gameObject);
         var resultSpeed = Speed * Time.deltaTime;
         if (Physics.Raycast(transform.position, transform.forward, out var hit, resultSpeed * 1.25f, Layers, QueryTriggerInteraction.Collide)) {
             var target = hit.transform.gameObject;
-            if (Owner == target.gameObject)
-                return;
-            if (Owner.GetComponent<Bot>() != null && target.GetComponent<Bot>() != null)
-                return; //hack
-            if (Owner.GetComponent<SecurityBot>() != null && target.GetComponent<SecurityBot>() != null 
-                || Owner.GetComponent<SecurityBot>() != null && target.gameObject == PlayerController.Instance.gameObject)
-                return; //hack 2
-            if (Owner == PlayerController.Instance.gameObject && target.GetComponent<Module>() != null)
-                return; //hack 3
-            var health = target.GetComponent<Health>();
-            if (health != null)
-                health.HealthCurrent -= Damage;
-            Destroy(gameObject);
+            if (CheckTargetValid(target)) {
+                var health = target.GetComponent<Health>();
+                if (health != null)
+                    health.HealthCurrent -= Damage;
+                Destroy(gameObject);
+            }
         }
         transform.position += transform.forward * resultSpeed;
-        if (Time.time - _SpawnTime > Lifetime)
-            Destroy(gameObject);
+
+    }
+
+    private bool CheckTargetValid(GameObject target) {
+        if (Owner == target.gameObject)
+            return false;
+        if (Owner.GetComponent<Bot>() != null && target.GetComponent<Bot>() != null)
+            return false;
+        if (Owner.GetComponent<SecurityBot>() != null && target.GetComponent<SecurityBot>() != null
+            || Owner.GetComponent<SecurityBot>() != null && target.gameObject == PlayerController.Instance.gameObject)
+            return false;
+        if (Owner == PlayerController.Instance.gameObject && target.GetComponent<Module>() != null)
+            return false;
+        return true;
     }
 }
