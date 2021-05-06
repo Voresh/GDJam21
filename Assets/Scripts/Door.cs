0d1f18ce39d876744d56f369d48c14891fbb16e1
Oreshkov Vladimir <voresh@inbox.ru>
2021-05-06 17:03:06 +0300

    door pirce

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,20 +1,39 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Door : MonoBehaviour, IRepairable {
+    public PriceBar PriceBarPrefab;
     public Renderer Renderer;
     public NavMeshObstacle NavMeshObstacle;
     public List<Module> ModulesToUnlock;
     public List<GameObject> RespawnesToUnlock;
     public List<Door> DoorsToActivate;
     public int InitialRepairPrice = 5;
-
+    private PriceBar _PriceBar;
+    public float PriceHeightOffset = 2f;
+    
     public Vector3 RepairablePosition => transform.position;
     public bool Repaired { get; set; }
     public int RepairPrice => InitialRepairPrice;
-    
+
+    private void Start() {
+        if (PriceBarPrefab != null) {
+            _PriceBar = Instantiate(PriceBarPrefab);
+            _PriceBar.Price = RepairController.Instance.GetRepairPrice(this);
+        }
+    }
+
+    private void Update() {
+        if (_PriceBar != null) {
+            _PriceBar.transform.position = transform.position + Vector3.up * PriceHeightOffset;
+            _PriceBar.Text.text = RepairController.Instance.GetRepairPrice(this).ToString();
+        }
+    }
+
     public void Repair() {
+        _PriceBar.gameObject.SetActive(false);
         Renderer.enabled = false;
         NavMeshObstacle.enabled = false;
         Repaired = true;
