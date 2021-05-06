5fcabe454a2f77d5f77ee2f9bb41cf1b8b75fc59
Oreshkov Vladimir <voresh@inbox.ru>
2021-05-06 20:48:08 +0300

    arrows

--- Assets/Scripts/Widgets/StatusWidget.cs
+++ Assets/Scripts/Widgets/StatusWidget.cs
@@ -23,7 +23,7 @@ public class StatusWidget : MonoBehaviour {
 
     private void Update() {
         foreach (var (widget, module) in _Widgets) {
-            widget.Arrow.gameObject.SetActive(!module.Repaired);
+            //widget.Arrow.gameObject.SetActive(!module.Repaired);
             var targetRotation = WaveArrowWidget.GetRouteDirection(module.transform.position);
             widget.Arrow.rotation = Quaternion.Slerp(widget.Arrow.rotation, targetRotation, 10f * Time.deltaTime);
             if (module.Repaired)
